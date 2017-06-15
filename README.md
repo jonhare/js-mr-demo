dc0b2bac6470fef7c07b871e77016e9d910c5665
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2017-06-15 12:06:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-mr-demo
+
+A really simple node based web server for demonstrating the prinicples of distributed processing of large amounts of data. Part of an practical lab session on the benefits of functional programmming and Map Reduce approaches written for students of A-Level Computer Science.
---
+++ index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const {VM} = require('vm2');
+
+//configure ports, automatically choosing between the Heroku port and development port
+var httpPort = Number(process.env.PORT || 3000);
+
+var dir = "public/1/";
+var files = fs.readdirSync(dir);
+
+var data = []
+for (var i=0; i<files.length; i++) {
+	if (files[i].startsWith(".")) 
+		continue;
+
+	var content = fs.readFileSync(dir + files[i], 'utf8');
+	data.push(content);
+}
+
+app.get('/', function (req, res) {
+	res.send(JSON.stringify(data, null, 2));
+})
+
+app.get('/map', function (req, res) {
+	var vm = new VM({
+    	timeout: 1000,
+    	sandbox: {
+    		map: function(input) { return input; },
+    		data: data
+    	}
+	});
+	if (!req.query.fcn) req.query.fcn = '';
+
+	try {
+		var rawresponse = vm.run(req.query.fcn + 'data.map(map);');
+		res.send(JSON.stringify(rawresponse));
+	} catch (e) {
+		res.send(e.name);
+	}
+})
+
+app.listen(httpPort, function () {
+  console.log('Listening on port ' + httpPort);
+})
